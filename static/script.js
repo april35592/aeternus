a8d15d6166d73b71b0f60e4bd3aaf36896fd9283
윤은소 <ye37797@gmail.com>
2021-01-23 21:04:48 +0900

    id 삭제 등 코드 간략화

--- static/script.js
+++ static/script.js
@@ -1,9 +1,15 @@
-function toggleOpenClose(id,toggleId) {
-    if(document.getElementById(id).style.display == 'none') {
-        document.getElementById(id).style.display = 'block';
-        document.getElementById(toggleId).innerText = '△';
-    } else {
-        document.getElementById(id).style.display = 'none';
-        document.getElementById(toggleId).innerText = '▽';
+var reply_h3 = document.querySelectorAll(".reply_list h3");
+var reply_diraction = document.querySelectorAll(".reply_list span");
+var reply_p = document.querySelectorAll(".reply_list p");
+
+for (let i = 0; i < reply_h3.length; i++) {
+    reply_h3[i].onclick = function(event) {
+        if(reply_p[i].style.display == 'none') {
+            reply_p[i].style.display = 'block';
+            reply_diraction[i].innerText = '△';
+        } else {
+            reply_p[i].style.display = 'none';
+            reply_diraction[i].innerText = '▽';
+        }
     }
 }
