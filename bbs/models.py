64138d7f3d60f27be8b24c5226c6242a2355e860
april35592 <77724156+april35592@users.noreply.github.com>
2021-02-01 06:06:37 +0900

    Update models.py

--- bbs/models.py
+++ bbs/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Guest(models.Model):
     name = models.CharField('NAME', max_length=20)
     content = models.TextField()
-    date = models.DateTimeField()
+    date = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return '%s %s' % (self.date, self.name)
 
